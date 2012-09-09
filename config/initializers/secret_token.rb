1412028d508e7a9058376e806c4a95e389ab2249
ilya.kuzmin <ilya.kuzmin@flatsoft.com>
2012-09-09 22:42:46 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metaapp::Application.config.secret_token = '67f627fefc4bdea8aa33c219c56e7e9025f238e0e094be9d6fed9212a6e45cedc91de991e1c038e0606ba2336cd57e31a0e7e202cd8fcd92b97512a3b15185ed'
